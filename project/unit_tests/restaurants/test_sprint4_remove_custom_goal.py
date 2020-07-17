ecbecd5e244f3367424d690b9b3f3ed7a8465bc3
tiajang <tmwjang@gmail.com>
2020-07-17 15:21:27 -0400

    fixed unit test

--- project/unit_tests/restaurants/test_sprint4_remove_custom_goal.py
+++ project/unit_tests/restaurants/test_sprint4_remove_custom_goal.py
@@ -1,15 +1,23 @@
 """
-This file houses the unit test suite for the edit restaurant profile feature.
+This file houses the unit test for remoce custom goal backend.
 """
 
 import os
 import sys
 import pytest
+import random 
+from random import randint
 sys.path.insert(1, os.path.join(os.path.dirname(__file__),
                                 '../../src'))  # Import the src folder
 from restaurants_app import app
 from modules.restaurant_profile_manager import RestaurantProfileManager
 
+def create_app():
+    app = Flask(__name__)
+
+    with app.app_context():
+        init_db()
+    return app
 
 @pytest.fixture
 def client():
@@ -19,16 +27,18 @@ def client():
     """
     app.config['TESTING'] = True
     return app.test_client() 
-
+	
 def test_remove_custom_goal():
     """
     Test that the remove_custom_goal() function in 
     restaurant_profile_manager.py can be used to remove
     a user's custom goal.
     """
-    rpm = RestaurantProfileManager("vchang")
-    old_goals = rpm.get_custom_goals()
-    rpm.remove_custom_goal(old_goals[0]['_id'])
-    new_goals = rpm.get_custom_goals()
-    assert len(new_goals) < len(old_goals)
+    with app.app_context():
+        rpm = RestaurantProfileManager("vchang")
+        old_goals = rpm.get_custom_goals()
+        rand_int = random.randint(0, len(old_goals)-1)
+        rpm.remove_custom_goal(old_goals[rand_int]['_id'])
+        new_goals = rpm.get_custom_goals()
+        assert len(new_goals) == (len(old_goals) - 1)
 
