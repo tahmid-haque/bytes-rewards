3eccbefeab697f4eebaf09c54de05f56da7dc482
jodijang <joedjang@gmail.com>
2020-07-24 20:46:19 -0400

    Test cases and unit tests with their corresponding changes for add custom reward

--- project/src/routes/rewards/profile.py
+++ project/src/routes/rewards/profile.py
@@ -34,7 +34,7 @@ def view_board(obj_id):
               ) == obj_id and restaurant_user["profile"]["is_public"] is True:
             board_name = restaurant_user["bingo_board"]["name"]
             goal_ids = restaurant_user["bingo_board"]["board"]
-            user_rpm = RestaurantProfileManager(restaurant_user["username"]) 
+            user_rpm = RestaurantProfileManager(restaurant_user["username"])
             custom_goals_dict = user_rpm.get_custom_goals()
             for goal_id in goal_ids:
                 for goal in current_user.get_goals():
